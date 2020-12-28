df7e1da039a6ac43e9c10bb661a663c19714a470
kajoldhaubanjar14 <52886593+Susansujakhu@users.noreply.github.com>
2020-12-29 00:28:37 +0545

    updated

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:assignment_2/screens/feeds.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.green, systemNavigationBarColor: Colors.green));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Feeds(),
+    );
+  }
+}
---
+++ lib/screens/feeds.dart
@@ -0,0 +1,27 @@
+import 'package:assignment_2/screens/feedController.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Feeds extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appbar(),
+      body: GetBuilder(
+        init: FeedController(),
+        builder: (FeedController controller) => Container(
+          child: ListView(
+            children: controller.cards,
+          ),
+        ),
+      ),
+    );
+  }
+
+  _appbar() {
+    return AppBar(
+      backgroundColor: Colors.green,
+      title: Text("API"),
+    );
+  }
+}
